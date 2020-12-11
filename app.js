07e80c8d552cef18d5652d8bcee454c8b25461f5
DanishOnGit <youcandoit1911@gmail.com>
2020-12-11 20:21:04 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var userInput=document.querySelector("#inputDate")
+var clickBtn=document.querySelector("#btnCheck")
+var ouput=document.querySelector("#result")
+
+
+function checkInputValid(){
+
+var split=userInput.split("/")
+var date=split[0];
+
+
+
+
+
+}
