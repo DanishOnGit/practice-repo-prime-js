6d8fcb55eb7a3f92ad063551195f55bdde0ad277
DanishOnGit <youcandoit1911@gmail.com>
2020-12-11 21:57:31 +0530

    added js file

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ function checkInputValid(){
 
 var split=userInput.split("/")
 var date=split[0];
+var month=split[1];
+var year=split[2];
+if(isNaN(date)||isNaN(month)||isNaN(year)){
+
+    alert("Please enter a valid DOB.")
+    checkInputValid()
+}else if(date<0||date>31||month<0||month>12||year<1000||year>9999){
+    alert("please enter valid DOB.")
+}
+
 
 
 
 
 
 }
+clickBtn.addEventListener("click",checkInputValid)
