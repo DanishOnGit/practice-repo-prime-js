f6cbf908c9f9c0d3226584de2eeaf62e2c2c6711
DanishOnGit <youcandoit1911@gmail.com>
2020-12-12 00:06:11 +0530

    fix: repetitive alert bug

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 var userInput=document.querySelector("#inputDate")
 var clickBtn=document.querySelector("#btnCheck")
-var ouput=document.querySelector("#result")
+var output=document.querySelector("#result")
+
+var text1="Date is neither prime nor composite"
+var text2="Date of birth is not prime number."
+var text3="Date of birth is prime number."
+
 
 
 function checkInputValid(){
-
-var split=userInput.split("/")
-var date=split[0];
-var month=split[1];
-var year=split[2];
+    var inputDob=userInput.value;
+    console.log(inputDob)
+var split=inputDob.split("/")
+console.log(split[0])
+  var date=split[0];
+  var month=split[1];
+  var year=split[2];
 if(isNaN(date)||isNaN(month)||isNaN(year)){
 
-    alert("Please enter a valid DOB.")
-    checkInputValid()
+   
+    console.log("enter sumthing valid")
+    
 }else if(date<0||date>31||month<0||month>12||year<1000||year>9999){
-    alert("please enter valid DOB.")
+    
+    
+    console.log("enter sumthing valid")
+}else if(!Number.isInteger(date) || !Number.isInteger(month)||!Number.isInteger(year)){
+   
+    console.log("enter sumthing valid")
+
+}
+   else if(date===1){
+    output.innerText=text1;
+    
+}    else{
+    for(var i=2;i<=date;i++){
+        if(date%i===0){
+            output.innerText=text2;
+
+        }else{
+            output.innerText=text3;
+        }
+    }
+
+}
+
 }
 
 
-
-
-
-
-}
 clickBtn.addEventListener("click",checkInputValid)
