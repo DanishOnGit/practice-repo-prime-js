9b1f67ada106ccb0ce9eeefa2f21ef297b00f1b0
DanishOnGit <youcandoit1911@gmail.com>
2020-12-12 12:53:45 +0530

    test: various invalid input cases
    added code to test various invalid input cases

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var output=document.querySelector("#result")
 var text1="Date is neither prime nor composite"
 var text2="Date of birth is not prime number."
 var text3="Date of birth is prime number."
-
+ var monthDays=[31,29,31,30,31,30,31,31,30,31,30,31]
 
 
 function checkInputValid(){
+    flag=false;
     var inputDob=userInput.value;
-    console.log(inputDob)
+    // console.log(inputDob)
 var split=inputDob.split("/")
 console.log(split[0])
   var date=split[0];
@@ -18,25 +19,39 @@ console.log(split[0])
   var year=split[2];
 if(isNaN(date)||isNaN(month)||isNaN(year)){
 
+   console.log("enter sumthing valid")
+   invalidInput()
    
-    console.log("enter sumthing valid")
     
-}else if(date<0||date>31||month<0||month>12||year<1000||year>9999){
-    
-    
-    console.log("enter sumthing valid")
-}else if(!Number.isInteger(date) || !Number.isInteger(month)||!Number.isInteger(year)){
-   
-    console.log("enter sumthing valid")
-
 }
-   else if(date===1){
+   else if(date<=0||date>31||month<=0||month>12||year<1000||year>9999){
+
+    console.log("enter sumthing valid")
+    invalidInput()
+   
+}
+else if(date>monthDays[month-1]){
+    console.log("enter sumthing valid")
+    invalidInput()
+}
+   
+// else if(!Number.isInteger(Number(`${date}`)) || !Number.isInteger(Number(`${month}`))||!Number.isInteger(Number(`${year}`))){
+
+//     console.log("enter sumthing valid")
+//     invalidInput()
+
+//    return
+    
+
+// }
+    if(date==1){
     output.innerText=text1;
     
 }    else{
-    for(var i=2;i<=date;i++){
-        if(date%i===0){
+    for(var i=2;i<date;i++){
+        if(date%i==0){
             output.innerText=text2;
+            break;
 
         }else{
             output.innerText=text3;
@@ -49,3 +64,7 @@ if(isNaN(date)||isNaN(month)||isNaN(year)){
 
 
 clickBtn.addEventListener("click",checkInputValid)
+
+function invalidInput(){
+    alert("Invalid Input")
+}
